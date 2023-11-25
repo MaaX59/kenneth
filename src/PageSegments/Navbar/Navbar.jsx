fd506cb9152d02077d8c5640016aeac331df090a
MaaX59 <Maax.strandberg@gmail.com>
2023-11-25 21:01:13 +0100

    small changes

--- src/PageSegments/Navbar/Navbar.jsx
+++ src/PageSegments/Navbar/Navbar.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-  return (
-    <div class="mx-5">Navbar</div>
-  )
-}
+  return <div class="mx-5 text-red-500 p-10 text-lg ">Navbar</div>;
+};
 
-export default Navbar
+export default Navbar;
