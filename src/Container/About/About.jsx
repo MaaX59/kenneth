0f6fa467e8ebf2e8d717326ec5c0cc9aa410d0d6
MaaX59 <Maax.strandberg@gmail.com>
2023-11-27 22:13:01 +0100

    attempt at snowfall

--- src/Container/About/About.jsx
+++ src/Container/About/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import headshot from '../../Images/headshot.jpg'
+import Snowfall from 'react-snowfall'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      
+      <div><Snowfall class="position: absolute top-20 h-52"/></div>
+      <div class=" w md:w-100">
+        <img src={headshot} alt="headshot" />
+      </div>
+      <div></div>
+
+    </div>
   )
 }
 
