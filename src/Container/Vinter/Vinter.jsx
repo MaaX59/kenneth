2fd228e51e2ecf8ab50711ed538fe0a710636c15
MaaX59 <Maax.strandberg@gmail.com>
2023-11-29 19:29:09 +0100

    slider dont work

--- src/Container/Vinter/Vinter.jsx
+++ src/Container/Vinter/Vinter.jsx
@@ -8,24 +8,24 @@ import image5 from '../../Images/vinter/05.jpg'
 
 const Vinter = () => {
   return (
-      <div classname="container"class="flex justify-center items-center h-auto w-full">
+      <div class="flex justify-center items-center  w-full">
     
-      <div classname="slider" class="flex w-5/6 h-5/6 ">
-        <div classname="slide 1" class="slide">
-            <img src={image1} alt="first "/>
+      <div  class="flex w-5/6 h-5/6 ">
+        <div  class="slide bg-1">
+            {/* <img src={image1} alt="first "/> */}
           
         </div>
-        <div classname="slide 2" class="slide">
-        <img src={image2} alt="first "/>
+        <div  class="slide bg-2 ">
+        {/* <img src={image2} alt="first "/> */}
         </div>
-        <div classname="slide 3" class="slide">
-        <img src={image3} alt="first "/>
+        <div  class="slide bg-3 ">
+        {/* <img src={image3} alt="first "/> */}
         </div>
-        <div classname="slide 4" class="slide">
-        <img src={image4} alt="first "/>
+        <div  class="slide bg-4 ">
+        {/* <img src={image4} alt="first "/> */}
         </div>
-        <div classname="slide 5" class="slide">
-        <img src={image5} alt="first "/>
+        <div  class="slide bg-5 ">
+        {/* <img src={image5} alt="first "/> */}
         </div>
       </div>
     </div>
