75b6f57ea44a456a3135436db4108aa372e29e41
MaaX59 <Maax.strandberg@gmail.com>
2023-11-29 18:47:21 +0100

    attempt at flexible slider

--- src/Container/Vinter/Vinter.jsx
+++ src/Container/Vinter/Vinter.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
+import image1 from '../../Images/vinter/01.jpg'
+import image2 from '../../Images/vinter/02.jpg'
+import image3 from '../../Images/vinter/03.jpg'
+import image4 from '../../Images/vinter/04.jpg'
+import image5 from '../../Images/vinter/05.jpg'
+
 
 const Vinter = () => {
   return (
-      <div class="flex justify-center items-center h-100% w-100%">
-    <div class="slider-container">
-
-      <div class="slider">
-        <div class="slide majin">
+      <div classname="container"class="flex justify-center items-center h-auto w-full">
+    
+      <div classname="slider" class="flex w-5/6 h-5/6 ">
+        <div classname="slide 1" class="slide">
+            <img src={image1} alt="first "/>
           
         </div>
-        <div class="slide krillin">
-          
+        <div classname="slide 2" class="slide">
+        <img src={image2} alt="first "/>
         </div>
-        <div class="slide android18">
-          
+        <div classname="slide 3" class="slide">
+        <img src={image3} alt="first "/>
         </div>
-        <div class="slide android17">
-          
+        <div classname="slide 4" class="slide">
+        <img src={image4} alt="first "/>
         </div>
-        <div class="slide goku">
-          
+        <div classname="slide 5" class="slide">
+        <img src={image5} alt="first "/>
         </div>
       </div>
     </div>
-  </div>
+  
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
         94:'94%'
       },
       backgroundImage:{
-          'background':[' linear-gradient(156deg, rgba(0,0,0,1) 45%, rgba(74,74,77,1) 91%);']
+        1: "url('https://images.unsplash.com/photo-1533468432434-200de3b5d528?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80')",
+        2: "url('https://images.unsplash.com/photo-1586254574632-55e4aaea7793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')",
+        3: "url('https://images.unsplash.com/photo-1536536982624-06c1776e0ca8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')",
+        4: "url('https://images.unsplash.com/photo-1502827186494-9f7976a04548?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=976&q=80')",
+        5: "url('https://images.unsplash.com/photo-1545559054-8f4f9e855781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')",
+      },
+      flex: {
+        5: 5,
       },
       fontFamily:{
         'dancing':[ 'Dancing Script', 'cursive']
