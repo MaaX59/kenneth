2ead3be7f5e6912a75e9466843a1febbe4a961bd
MaaX59 <Maax.strandberg@gmail.com>
2023-12-01 23:00:58 +0100

    pop up started

--- src/Container/Vinter/Vinter.jsx
+++ src/Container/Vinter/Vinter.jsx
@@ -7,43 +7,43 @@ import image5 from "../../Images/vinter/05.jpg";
 
 const Vinter = () => {
   const images = [image1, image2, image3, image4, image5];
-  const [fullImage, setFullImage] = useState(false);
-  const [image, setImage] = useState();
+  const [popup, setPopup] = useState(false);
+  const [fullImage, setFullImage] = useState();
 
   const openImage = (image) => {
-    setImage(image)
-    setFullImage(true);
-
+    setFullImage(image);
+    setPopup(!popup);
+    console.log(fullImage);
   };
 
   return (
     <div class="flex items-center justify-center flex-col ">
       <div>
-        <h2 class="text-gray-500 flex text-5xl text-opacity-1 p-10">Vinter</h2>
+        <h2 class="text-gray-500 flex text-5xl text-opacity-1 p-14">Vinter</h2>
       </div>
-      <div class="flex flex-wrap justify-center">
-        {images.map((image) => (
-          <div
-            onClick={(image) => openImage(image)}
-            class=" w-64 p-5 hover:scale-125 transition"
-          >
+      <div class="flex flex-wrap justify-center gap-12 z-10">
+        {images.map((image, index) => (
+          <div class=" w-64 hover:scale-125 transition">
             <img
               src={image}
-              alt="adad"
+              alt={index}
+              onClick={(image) => {
+                openImage(image);
+              }}
               class="rounded duration-75 cursor-pointer"
             />
           </div>
         ))}
       </div>
-{fullImage ? <div class="h-100% w-100% fixed bg-black opacity-30">
-  <div class="fixed justify-center items-center ">
 
-  </div>
-   <div/> : null
-};
+      {popup && (
+        <div class="h-max w-max absolute bg-gray-500 top-1/2 left-1/2 z-50">
+          <div class="fixed justify-center items-center ">
+            <img src={fullImage} alt="full" />
+          </div>
+        </div>
+      )}
     </div>
-
-    
   );
 };
 
